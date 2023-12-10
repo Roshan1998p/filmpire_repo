4009c07f80b07ca6bedeefa10ba544dec387137c
imRoshan_98 <roshan.1998p@gmail.com>
2023-12-10 22:26:28 +0530

    123

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,32 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "airbnb",
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
+    parser: "babel-eslint",
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react"],
   rules: {
+    "spaced-comment": [
+      "error",
+      "never",
+      {
+        block: {
+          balanced: true,
+        },
+      },
+    ],
     "import/no-cycle": 0,
     "no-console": 0,
     "react/prop-types": 0,
@@ -29,15 +38,18 @@ module.exports = {
     "max-len": [2, 250],
     "object-curly-newline": 0,
     "react/jsx-filename-extension": 0,
-    "react/react-in-jsx-scope": 0,
-    "no-sparse-arrays": 0,
     "react/jsx-one-expression-per-line": 0,
-    "jsx-ally/click-events-have-key-events": 0,
-    "jsx-ally/alt-text": 0,
-    "jsx-ally/no-autofocus": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/alt-text": 0,
+    "jsx-a11y/no-autofocus": 0,
     "jsx-a11y/no-static-element-interactions": 0,
     "react/no-array-index-key": 0,
     "no-param-reassign": 0,
+    "react/react-in-jsx-scope": 0,
     "react/jsx-props-no-spreading": 0,
+    "no-sparse-arrays": 0,
+    "no-array-index-key": 0,
+    camelcase: 0,
+    quotes: [2, "single", "avoid-escape"],
   },
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <h1>App 2</h1>
+    </div>
+  );
 };
 export default App;
