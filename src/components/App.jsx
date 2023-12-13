35c231980692d764e7c3acb20d9dcd71f9f4b9e5
imRoshan_98 <roshan.1998p@gmail.com>
2023-12-13 22:49:49 +0530

    vxcvx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <CssBaseline />
-      <h1>App 2</h1>
+      <main>
+        <Routes>
+          {/* Routes in this app don't need to worry about which URL prefix they are
+          mounted at. They can just assume they are mounted at /. Then, if they
+          are moved under a different basename later on, all routes and links will
+          continue to work. */}
+          <Route path="/Home" element={<h1>Hiisdfa sdfasdfasii</h1>}></Route>
+          <Route path="/" element={<h1>Hiiii 2423 23234</h1>}></Route>
+          <Route path="/" element={<h1>Hiii 42 ttti</h1>}></Route>
+        </Routes>
+      </main>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* "Mount" this app under the /inbox URL pathname. All routes and links
+        are relative to this name. */}
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
