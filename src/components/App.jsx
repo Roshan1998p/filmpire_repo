8882254159e5f61e0f45b3cdbd088d14f9d7afb7
imRoshan_98 <roshan.1998p@gmail.com>
2023-12-13 23:42:24 +0530

    files addded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
+import { Actors, NavBar, MovieInformation, Profile, Movies } from './index.js';
+import useStyles from './style';
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
-      <main>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           {/* Routes in this app don't need to worry about which URL prefix they are
           mounted at. They can just assume they are mounted at /. Then, if they
           are moved under a different basename later on, all routes and links will
           continue to work. */}
-          <Route path="/Home" element={<h1>Hiisdfa sdfasdfasii</h1>}></Route>
-          <Route path="/" element={<h1>Hiiii 2423 23234</h1>}></Route>
-          <Route path="/" element={<h1>Hiii 42 ttti</h1>}></Route>
+          <Route
+            path="/movie/:id"
+            element={
+              <h1>
+                <MovieInformation />
+              </h1>
+            }
+          ></Route>
+          <Route exact path="/actors/:id" element={<Actors />}></Route>
+          <Route path="/" element={<Movies />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
         </Routes>
       </main>
     </div>
